63dbf4971274b4a82acc565564bbd81dccd77258
Sybatron <vipsister2014@gmail.com>
2024-11-02 12:59:35 +0200

    implemented order of the phoenix voting system

---
+++ entry-exam/t02-order-of-phoenix-voting.js
@@ -0,0 +1,49 @@
+//@ts-check
+
+/**
+ * 02 - Order of the Phoenix Votes
+ * @param {string[]} votes array of votes
+ */
+function getOrderDecision(votes) {
+    let votingScore = 0;
+    let hasNonAbstain = false;
+    for (let ind = 0; ind < votes.length; ind++) {
+        switch (votes[ind].toLowerCase()) {
+            case "yes":
+                votingScore++;
+                hasNonAbstain = true;
+                break;
+            case "no":
+                votingScore--;
+                hasNonAbstain = true;
+                break;
+            case "abstain":
+                //stays the same
+                break;
+        }
+    }
+
+    if (!hasNonAbstain) {
+        return "Abstain";
+    }
+    if (votingScore > 0) {
+        return "Yes";
+    }
+    if (votingScore < 0) {
+        return "No";
+    }
+    return "Tie";
+}
+
+//expected: Yes
+console.log(getOrderDecision(["Yes", "No", "Yes", "Abstain", "Yes"]));
+//expected: No
+console.log(getOrderDecision(["No", "No", "Yes"]));
+//expected: Tie
+console.log(getOrderDecision(["Yes", "No"]));
+//expected: Abstain
+console.log(getOrderDecision(["Abstain"]));
+//expected: Tie
+console.log(getOrderDecision(["No", "No", "Abstain", "Abstain", "Yes", "Yes"]));
+//expected: No
+console.log(getOrderDecision(["No", "Abstain", "Abstain"]));
