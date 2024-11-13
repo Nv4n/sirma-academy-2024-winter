e953fd81b8de069dc45fa3333da776a3596eee0d
Sybatron <vipsister2014@gmail.com>
2024-11-13 10:52:29 +0200

    added chain constructor

---
+++ 13-advanced-functions/t02-chain-constructor.js
@@ -0,0 +1,14 @@
+//@ts-check
+
+//02 - Using call to Chain Constructors
+
+function Person(name) {
+    this.name = name;
+}
+function Employee(name, job) {
+    Person.call(this, name);
+    this.job = job;
+}
+
+const emp = new Employee("Alice", "Engineer");
+console.log(emp); // Output: Employee { name: 'Alice', job: 'Engineer' }
