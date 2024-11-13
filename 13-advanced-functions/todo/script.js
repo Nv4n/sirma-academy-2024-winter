22946aa8fd27bea9ed86ae3bb6546428fa2432b9
Sybatron <vipsister2014@gmail.com>
2024-11-13 12:13:51 +0200

    added todo list task

--- 13-advanced-functions/todo/script.js
+++ 13-advanced-functions/todo/script.js
@@ -1,24 +1,68 @@
-// classic
-const addBtn = document.getElementById("add-button");
-const inputField = document.getElementById("todo-input");
+//@ts-check
 
-addBtn.addEventListener("click", () => {
-    console.log(
-        "🚀 ~ addBtn.addEventListener ~ inputField.value:",
-        inputField.value.trim()
-    );
-});
+// classic
+// const addBtn = document.getElementById("add-button");
+// const inputField = document.getElementById("todo-input");
+
+// addBtn.addEventListener("click", () => {
+//     console.log(
+//         "🚀 ~ addBtn.addEventListener ~ inputField.value:",
+//         inputField.value.trim()
+//     );
+// });
 
 // secure
 
-// (() => {
-//     const addBtn = document.getElementById("add-button");
-//     const inputField = document.getElementById("todo-input");
+(() => {
+    const addTodoBtn = document.getElementById("add-button");
+    const todoInput = document.getElementById("todo-input");
+    const todoList = document.getElementById("todo-list");
 
-//     addBtn.addEventListener("click", () => {
-//         console.log(
-//             "🚀 ~ addBtn.addEventListener ~ inputField.value:",
-//             inputField.value.trim()
-//         );
-//     });
-// })();
+    if (!addTodoBtn) {
+        throw new Error("No add button found!");
+    }
+    if (!todoInput) {
+        throw new Error("No input field found!");
+    }
+    if (!todoList) {
+        throw new Error("No todo list found!");
+    }
+
+    addTodoBtn.addEventListener("click", () => {
+        addNewTodo(todoInput, todoList);
+
+        console.log(
+            "🚀 ~ addBtn.addEventListener ~ inputField.value:",
+            // @ts-ignore
+            todoInput.value.trim()
+        );
+
+        // @ts-ignore
+        todoInput.value = "";
+    });
+})();
+
+/**
+ *
+ * @param {HTMLElement} inputField
+ * @param {HTMLElement} list
+ */
+function addNewTodo(inputField, list) {
+    const item = document.createElement("li");
+
+    const text = document.createElement("span");
+    // @ts-ignore
+    text.textContent = inputField.value.trim();
+
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = "REMOVE";
+    removeBtn.addEventListener("click", () => {
+        list.removeChild(item);
+    });
+    removeBtn.classList.add("remove-button");
+
+    item.appendChild(text);
+    item.appendChild(removeBtn);
+
+    list.appendChild(item);
+}
