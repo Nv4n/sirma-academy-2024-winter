3bb7b9d914af5048727ccd0f1cc36344b5c3c335
Sybatron <vipsister2014@gmail.com>
2024-11-13 11:58:51 +0200

    added fibonacci with closures

---
+++ 13-advanced-functions/t09-fibonacci.js
@@ -0,0 +1,24 @@
+//@ts-check
+
+// 09 - Fibonacci
+
+function getFibonacci() {
+    let prev = 0;
+    let curr = 1;
+    return () => {
+        let buffer = curr;
+        curr += prev;
+        prev = buffer;
+        return buffer;
+    };
+}
+
+let fibonacci = getFibonacci();
+console.log(fibonacci()); // 1
+console.log(fibonacci()); // 1
+console.log(fibonacci()); // 2
+console.log(fibonacci()); // 3
+console.log(fibonacci()); // 5
+console.log(fibonacci()); // 8
+console.log(fibonacci()); // 13
+console.log(fibonacci()); // 21
